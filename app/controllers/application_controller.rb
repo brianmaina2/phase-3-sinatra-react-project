4b3bc0041436c98b405bc2a18fa898996a646dfa
brian.maina@student.moringaschool.com <brianmaina2>
2023-03-06 01:13:09 +0300

    Completed Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,64 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/recipes' do
+  recipe = Recipe.all
+  recipe.to_json
   end
 
+  get '/recipes/:id' do 
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.to_json
+  end
+  
+  post '/recipes' do
+  recipe = Recipe.create(
+  name: params[:name],
+  ingredients: params[:ingredients],
+  cooking_time: params[:cooking_time],
+  instructions: params[:instructions]
+  )
+  recipe.to_json
+  end
+  
+
+  delete '/recipes/:id' do
+  recipe = Recipe.find_by(id: params[:id])
+  if recipe
+  recipe.destroy
+  status 204
+  else
+  status 404
+  end
+  end 
+  # This route returns a list of all the users.
+get '/users' do
+  recipe = User.all
+  recipe.to_json
+  end
+
+
+# This route creates a new user.
+post '/users' do
+user = User.create(
+email: params[:email],
+password: params[:password]
+)
+user.to_json
 end
+
+# This route deletes a user with the specified ID.
+delete '/users/:id' do
+user = User.find_by(id: params[:id])
+if user
+user.destroy
+status 204
+else
+status 404
+end
+end
+
+
+
+  end
+
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 puts "🌱 Seeding spices..."
+require 'active_record'
+require_relative '../config/environment'
 
-# Seed your database here
-
-puts "✅ Done seeding!"
+10.times do
+User.create(
+email: Faker::Internet.email,
+password:Faker::Internet.password
+)
+end
+10.times do
+Recipe.create(
+name: Faker::Food.dish,
+ingredients: Faker::Food.ingredient,
+cooking_time: rand(10..120),
+instructions: Faker::Food.description,
+user_id: rand(1..10)
+)
+end
